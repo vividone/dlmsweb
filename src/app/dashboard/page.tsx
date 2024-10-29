ef8a9b5f78c09ff29189357b97bbac06913d1bd4
pascal <pascalokereke18@gmail.com>
2024-10-29 21:42:28 +0100

    dashboard 90% complete

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -29,16 +29,16 @@ const dashboardTwos = [
   { id: 1, title: 'Harry Potter', genre: 'Adventure', cover: "/harry potter.jpg", borrowStatus: 'Returned', returnDate: '2024-10-10', borrowedDate: '2024-09-15' },
   { id: 2, title: 'Hide and seek', genre: 'Thriller', cover: "/Hide and seek.jpg", borrowStatus: 'Borrowed', returnDate: 'N/A', borrowedDate: '2024-10-01' },
   { id: 3, title: 'Spring Book', genre: 'Romance', cover: "/spring book.jpg", borrowStatus: 'Returned', returnDate: '2024-09-30', borrowedDate: '2024-09-10' },
-  { id: 4, title: 'Lone Wolf Adventure', genre: 'Adventure', cover: "/Robin lee.jpg", borrowStatus: 'Returned', returnDate: '2024-10-12', borrowedDate: '2024-09-18' },
+  { id: 4, title: 'Lone Wolf Adventure', genre: 'Adventure', cover: "/lone wolf.png", borrowStatus: 'Returned', returnDate: '2024-10-12', borrowedDate: '2024-09-18' },
   { id: 5, title: 'Walk in the shadow', genre: 'Fantasy', cover: "/walk in the shadow.jpg", borrowStatus: 'Borrowed', returnDate: 'N/A', borrowedDate: '2024-10-15' },
-  { id: 6, title: 'All This Time', genre: 'Adventure', cover: "/All This Time.jpg", borrowStatus:'Returned', returnDate: 'N/A', borrowedDate: '2024-10-12'},
+  { id: 6, title: 'All This Time', genre: 'Adventure', cover: "/All This Time.png", borrowStatus:'Returned', returnDate: 'N/A', borrowedDate: '2024-10-12'},
 ]
 
 const dashboardThrees = [
-  { id: 1, title: 'All This Time', genre: 'Adventure', cover: "/All This Time.jpg", borrowStatus: 'Returned', returnDate: '2024-10-10', borrowedDate: '2024-09-15' },
+  { id: 1, title: 'All This Time', genre: 'Adventure', cover: "/All This Time.png", borrowStatus: 'Returned', returnDate: '2024-10-10', borrowedDate: '2024-09-15' },
   { id: 2, title: 'Tigers Heart', genre: 'Thriller', cover: "/Tigers heart.jpg", borrowStatus: 'Borrowed', returnDate: 'N/A', borrowedDate: '2024-10-01' },
   { id: 3, title: 'Walk in the shadow', genre: 'Fantasy', cover: "/walk in the shadow.jpg", borrowStatus: 'Returned', returnDate: '2024-09-30', borrowedDate: '2024-09-10' },
-  { id: 4, title: 'Robin lee', genre: 'Thriller', cover: "/lone wolf.jpg", borrowStatus: 'Returned', returnDate: '2024-10-12', borrowedDate: '2024-09-18' },
+  { id: 4, title: 'Robin lee', genre: 'Thriller', cover: "/Robin lee.jpg", borrowStatus: 'Returned', returnDate: '2024-10-12', borrowedDate: '2024-09-18' },
   { id: 5, title: 'Don\'t Look Back', genre: 'Thriller', cover: "/Dont Look.png", borrowStatus: 'Borrowed', returnDate: 'N/A', borrowedDate: '2024-10-15' },
   { id: 6, title: 'Norse Myth', genre: 'Fantasy', cover: "/Norse Myth.jpg", borrowStatus:'Returned', returnDate: 'N/A', borrowedDate: '2024-10-12'},
 ]
@@ -58,7 +58,10 @@ export default function Dashboard() {
    });
 
 
-  const combinedFilteredBooks = [...filterData(books), ...filterData(dashboardTwos)];
+  const combinedFilteredBooks = 
+  [...filterData(books), 
+   ...filterData(dashboardTwos), 
+   ...filterData(dashboardThrees)];
   setFilteredBooks(combinedFilteredBooks);
   }, [searchTerm]);
 
@@ -194,11 +197,12 @@ export default function Dashboard() {
           <option value="All">Date Borrowed</option>
         </select>
       </div>
-
-
-
     
+
+
       {/* Book List */}
+      {/*First row */}
+
       <div className="grid w-[1297px] h-[209px]">
       <div className="flex gap-8 relative font-sans bottom-[200px]">
         {filteredBooks.map((book) => (
@@ -208,9 +212,36 @@ export default function Dashboard() {
               alt={book.title}
               className="rounded-md w-full h-[120px] cursor-pointer"
             />
-          </div>
-        
+          </div>       
         ))}
+      </div>
+
+      {/*second row */}
+      <div className="grid w-[1297px] h-[209px]">
+      <div className="flex gap-8 relative font-sans bottom-[200px]">
+        {dashboardTwos.map((book) => (
+          <div key={book.id} className="p-8 rounded-md">
+            <img 
+              src={book.cover}
+              alt={book.title}
+              className="rounded-md w-full h-[120px] cursor-pointer"
+            />
+          </div>       
+        ))}
+      </div>
+
+      {/*Third row */}
+      <div className="grid w-[1297px] h-[209px]">
+      <div className="flex gap-8 relative font-sans bottom-[200px]">
+        {dashboardThrees.map((book) => (
+          <div key={book.id} className="p-8 rounded-md">
+            <img 
+              src={book.cover}
+              alt={book.title}
+              className="rounded-md w-full h-[120px] cursor-pointer"
+            />
+          </div>
+     ))}
       </div>
   
       </div>
@@ -220,5 +251,7 @@ export default function Dashboard() {
      
         </div>
   </div>
+  </div>
+</div>
   );
 }
