6f7feab17d871c80ddc607dba614d03855cd979d
pascal <pascalokereke18@gmail.com>
2024-11-05 11:40:36 +0100

    rootlayout cover the fontfamily

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
     <html lang="en">
       <body
       
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${merriweatherSans.variable} antialiased`}
       >
         {children}
       </body>
