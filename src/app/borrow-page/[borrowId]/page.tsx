e68efef077c887817baccd7a58098b16aade394a
pascal <pascalokereke18@gmail.com>
2024-11-06 10:59:59 +0100

    the borrow.id for the borrow page.

--- src/app/borrow-page/[borrowId]/page.tsx
+++ src/app/borrow-page/[borrowId]/page.tsx
@@ -69,7 +69,7 @@ export default function BorrowId({ params }: { params: Promise<{borrowId: string
             <div className="flex items-center justify-between mb-8">
                 <h1 className="text-3xl font-bold text-[#0061E8]">BookaThon</h1>
                 <div className="flex items-center space-x-4">
-                    <Link href="/homepage" className="text-[#0061E8] font-semibold md:relative right-[640px] hover:text-blue-500">Library</Link>
+                    <Link href="/homepage" className="text-[#0061E8] font-semibold md:absolute right-[640px] hover:text-blue-500">Library</Link>
                     <Link href="/dashboard" className="font-semibold md:relative right-[640px] hover:text-blue-500">My Shelf</Link>
                     <FaBell className="text-lg text-gray-600 cursor-pointer hover:text-blue-500" />
                     <Image src="/user-avatar.jpg" alt="Avatar" width={40} height={20} className="w-8 h-8 border rounded-full cursor-pointer"
@@ -96,16 +96,16 @@ export default function BorrowId({ params }: { params: Promise<{borrowId: string
             {/* Borrow Section */}
             <Image src={borrow.cover} alt={borrow.title} width={270} height={50} className="mt-14 rounded-md" />
             <div className="flex flex-col items-center">
-                <h2 className="font-sans text-2xl font-semibold text-blue-900 md:absolute top-[180px]">You are borrowing:</h2>
-                <h3 className="text-xl font-semibold md:absolute top-[220px]">{borrow.title}</h3>
+                <h2 className="font-sans text-2xl font-semibold text-blue-900 md:absolute top-[180px] md:absolute left-[460px]">You are borrowing:</h2>
+                <h3 className="text-xl font-semibold md:absolute top-[220px] md:absolute left-[460px]">{borrow.title}</h3>
 
-                <form onSubmit={handleSubmit} className="flex flex-col items-center gap-4 mt-4 w-full max-w-md">
+                <form onSubmit={handleSubmit} className="flex flex-col gap-4 mt-12 w-full max-w-md md:absolute top-[250px] md:absolute left-[460px]">
                     <label className="font-sans text-sm font-semibold">Collection Date</label>
                     <input 
                         type="text" 
                         value={collectionDate}
                         placeholder="DD/MM/YYYY"
-                        className="border rounded-md p-2" 
+                        className="w-full px-4 py-3 border rounded-md" 
                         onChange={(e) => setCollectionDate(e.target.value)} 
                         required 
                     />
@@ -115,12 +115,12 @@ export default function BorrowId({ params }: { params: Promise<{borrowId: string
                         type="text" 
                         value={returnDate}
                         placeholder="DD/MM/YYYY"
-                        className="border rounded-md p-2" 
+                        className="w-full px-4 py-3 border rounded-md" 
                         onChange={(e) => setReturnDate(e.target.value)} 
                         required 
                     />
                     {error && <p className="text-red-500">{error}</p>}
-                    <div className="flex justify-centerp-8">
+                    <div className="flex justify-center p-8 items-center">
                         <button type="submit" className="bg-blue-600 w-72 text-white rounded-md items-center p-2 hover:bg-blue-700">
                             Submit
                         </button>
