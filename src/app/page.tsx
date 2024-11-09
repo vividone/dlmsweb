b9c8424fbe127382e5826689bdc9daf67bb01d17
pascal <pascalokereke18@gmail.com>
2024-11-09 08:56:10 +0100

    fix the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import Link from 'next/link';
-import { FaBell, FaSearch, FaFilter } from "react-icons/fa";
+import { FaBell, FaSearch, FaFilter, FaBars } from "react-icons/fa";
 import { useState, useEffect, useRef } from 'react';
 
 
@@ -16,21 +16,22 @@ interface Book {
   author: string;
 }
 
+
 const books: Book[] = [
-  { id: 1, title: "Lone Wolf Adventure", category: "Adventure", author: 'Emerngard Nausicaa', cover: "/lone wolf.png" },
-  { id: 2, title: "Hide and Seek", category: "Mystery", author: 'Olivia Wison', cover: "/Hide and seek.jpg" },
-  { id: 3, title: "Dont Look Back", category: "Thriller", author: 'Isaac Nelson', cover: "/Dont Look.png" },
+  { id: 1, title: "Lone Wolf Adventure", category: "Sci-fi", author: 'Emerngard Nausicaa', cover: "/lone wolf.png" },
+  { id: 2, title: "Hide and Seek", category: "Fantasy", author: 'Olivia Wison', cover: "/Hide and seek.jpg" },
+  { id: 3, title: "Dont Look Back", category: "Drama", author: 'Isaac Nelson', cover: "/Dont Look.png" },
   { id: 4, title: "Spring Book", category: "Romance", author: 'Deena Roberts', cover: "/spring book.jpg" },
-  { id: 5, title: "Harry Potter", category: "Fantasy", author: 'Isaac Nelson', cover: "/harry potter.jpg" },
-  { id: 6, title: "A Promise Kept", category: "Drama", author: 'Robert Lee Hatcher', cover: "/Robin lee.jpg" },
+  { id: 5, title: "Harry Potter", category: "Business", author: 'Isaac Nelson', cover: "/harry potter.jpg" },
+  { id: 6, title: "A Promise Kept", category: "Fantasy", author: 'Robert Lee Hatcher', cover: "/Robin lee.jpg" },
 ];
 
 const booksTwo: Book[] = [
-  { id: 7, title: "Dont Look Back", category: "Thriller", author: 'Isaac Nelson', cover: "/Dont Look.png" },
-  { id: 8, title: "Hide and Seek", category: "Mystery", author: 'Olivia Wison', cover: "/Hide and seek.jpg" },
-  { id: 9, title: "Harry Potter", category: "Fantasy", author: 'Isaac Nelson', cover: "/harry potter.jpg" },
-  { id: 10, title: "A Promise Kept", category: "Drama", author: 'Robert Lee Hatcher', cover: "/Robin lee.jpg" },
-  { id: 11, title: "Lone Wolf Adventure", category: "Adventure", author: 'Emerngard Nausica', cover: "/lone wolf.png" },
+  { id: 7, title: "Dont Look Back", category: "Drama", author: 'Isaac Nelson', cover: "/Dont Look.png" },
+  { id: 8, title: "Hide and Seek", category: "Fantasy", author: 'Olivia Wison', cover: "/Hide and seek.jpg" },
+  { id: 9, title: "Harry Potter", category: "Business", author: 'Isaac Nelson', cover: "/harry potter.jpg" },
+  { id: 10, title: "A Promise Kept", category: "Fantasy", author: 'Robert Lee Hatcher', cover: "/Robin lee.jpg" },
+  { id: 11, title: "Lone Wolf Adventure", category: "Sci-fi", author: 'Emerngard Nausica', cover: "/lone wolf.png" },
   { id: 12, title: "Spring Book", category: "Romance", author: 'Deena Roberts', cover: "/spring book.jpg" },
 ];
 
@@ -40,6 +41,7 @@ export default function Home() {
   const [filteredBooks, setFilteredBooks] = useState<Book[]>([]);
   const [selectedCategory, setSelectedCategory] = useState<string>('All');
   const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState<boolean>(false);
   const dropdownRef = useRef<HTMLDivElement>(null);
 
 
@@ -63,7 +65,7 @@ export default function Home() {
     document.addEventListener("mousedown", handleClickOutside);
       return () => document.removeEventListener("mousedown", handleClickOutside);
   }, []);
-  
+
   const handleClickOutside = (event: MouseEvent) => {
     if(dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
       setDropdownOpen(false);
@@ -71,28 +73,59 @@ export default function Home() {
   }
 
   return (
-    <div className="container mx-auto p-4 sm:p-6 lg:p-8">
+    <div className="container mx-auto p-4 overflow-x-hidden">
       {/* Header */}
-      <header className="flex flex-col sm:flex-row justify-between items-center mb-8 mb-6 space-y-4 sm:space-y-0">
+      <header className="flex justify-between items-center sm:flex-row mb-8 space-y-4 sm:space-y-0">
+      <div className="flex items-center space-x-8">
         <h1 className="text-3xl font-bold text-[#0661E8]">BookaThon</h1>
-        <div className="flex items-center space-x-4 mt-4 sm:mt-0">
-          <Link href="/homepage" className="text-[#0661E8] md:relative right-[640px] font-semibold text-sm sm:text-base hover:text-blue-500">
+          
+          {/*full nav links for larger screen */}
+          <nav className="hidden sm:flex space-x-6">
+          <Link href="/homepage" className="text-[#0061E8] text-base font-semibold sm:text-base hover:text-blue-500">
             Library
           </Link>
-          <Link href="/dashboard" className="font-semibold md:relative right-[600px] text-sm sm:text-base hover:text-blue-500">
+          <Link href="/dashboard" className="font-semibold text-base sm:text-base hover:text-blue-500">
             My Shelf
           </Link>
-          <FaBell className='text-xl text-gray-700 cursor-pointer hover:text-blue-500' />
+          </nav>
+
+          {/*Hamburger menu for mobile */}
+
+          <div className="sm:hidden flex items-center">
+            <FaBars className="text-md cursor-pointer"
+            onClick={() => setMenuOpen(!menuOpen)}
+            />
+            
+            {menuOpen && (
+            <div className="absolute top-16 left-4 right-4 bg-white border rounded-sm shadow-lg z-10">
+              <Link href="/homepage">
+                <div className="px-4 py-2 text-gray-700 hover:bg-gray-100 cursor-pointer">
+                  Library
+                </div>
+              </Link>
+              <Link href="/dashboard">
+                <div className="px-4 py-2 text-gray-700 hover:bg-gray-100 cursor-pointer">
+                  My Shelf
+                </div>
+              </Link>
+            </div>
+          )}
+          </div>
+          </div>
+
+        {/* Notification and Profile */}
+          <div className="flex items-center space-x-2 sm:space-x-4 absolute top-2 pr-6 right-0 sm:absolute top-2">
+          <FaBell className='text-sm text-gray-700 cursor-pointer hover:text-blue-500' />
           <Image  
             src='/user-avatar.jpg'
             alt="Avatar"
-            width={40}
-            height={20}
-            className="rounded-full w-8 h-8 cursor-pointer"
+            width={20}
+            height={10}
+            className="w-6 h-6 border rounded-full cursor-pointer"
             onClick={() => setDropdownOpen(!dropdownOpen)}
          />
          {dropdownOpen && (
-          <div ref={dropdownRef} className="absolute right-0 mt-2 w-48 bg-white border rounded-md shadow-lg">
+          <div ref={dropdownRef} className="absolute right-0 mt-2 w-48 sm:right-0 text-sm bg-white border rounded-md shadow-lg">
             <Link href='/sign-in'>
               <div className="px-4 py-2 text-gray-700 hover:bg-gray-100 cursor-pointer">
                 Sign In 
@@ -103,54 +136,54 @@ export default function Home() {
             </Link>
           </div>
          )}
-        </div>
+         </div>
       </header>
 
       {/* Find a Book */}
-      <div className="mb-6">
+      <div className="mb-6 ml-6">
         <p className="text-xl font-semibold">Find a Book</p>
       </div>
 
       {/* Search and Filter */}
-      <div className="flex flex-col sm:flex-row items-center justify-between mb-6 space-y-4 sm:space-y-0">
+      <div className="flex items-center mb-6 space-x-2 sm:space-x-4 md:space-x-4">
         <div className="relative flex-grow">
           <input 
             type="text" 
             value={searchTerm}
-            className="border p-3 sm:p-4 w-full rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"      
+            className="border text-sm opacity-40 p-3 sm:p-4 w-full rounded-md focus:outline-none focus:ring-2 focus:ring-gray-800"      
             placeholder="Type book name or author"
             onChange={(e) => setSearchTerm(e.target.value)}
           />
           <FaSearch 
-            className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 cursor-pointer" 
+            className="absolute text-sm right-3 top-1/2 transform -translate-y-1/2 text-gray-500 cursor-pointer" 
           />
         </div>
         <button 
           onClick={() => setSelectedCategory('All')}
-          className="flex items-center px-8 py-2 ml-4 bg-[#0661E8] text-white rounded-md hover:bg-blue-600"
+          className="flex items-center px-8 py-2 ml-4 text-white bg-[#0661E8] rounded-md hover:bg-blue-600"
         >
-          <FaFilter className="mr-2 text-lg opacity-40" /> {/* Filter Icon */}
-          Filter
+          <FaFilter className="mr-2 text-sm opacity-40" /> {/* Filter Icon */}
+         <p className="text-sm text-white">Filter</p>
         </button>
       </div>
 
       {/* Category Filter */}
-      <div className="flex flex-wrap justify-between font-semibold gap-4 sm:gap-4 mb-8 sm:justify-start">
-        {['All', 'Adventure', 'Drama', 'Thriller', 'Romance', 'Fantasy', 'Business', 'Education', 'Geography'].map((category) => (
+      <div className="flex overflow-x-auto flex lg:overflow-x-auto flex sm:overflow-visible gap-20 font-semibold sm:justify-between mb-6 animate-scroll">
+        {['All', 'Sci-fi', 'Fantasy', 'Romance', 'Drama', 'Business', 'Education', 'Geography'].map((category) => (
           <button
             key={category}
-            className={`px-4 py-2 rounded-md m-2 text-sm sm:text-base ${
+            className={`px-4 py-2 whitespace-nowrap lg:whitespace-nowrap rounded-md text-sm  ${
               selectedCategory === category ? 'bg-blue-600 text-white' : 'bg-blue-200 text-black'
             }`}
             onClick={() => setSelectedCategory(category)}
-          >
+          > 
             {category}
           </button>
         ))}
       </div>
 
       {/* Book Collection */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-6">
+      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-6">
         {filteredBooks.map((book) => (
           <Link key={book.id} href={`/book/${book.id}`}>
             <div className="p-4 rounded-md hover:shadow-lg transition-shadow cursor-pointer">
@@ -161,7 +194,7 @@ export default function Home() {
                 height={300}
                 className="rounded-md w-full h-auto"
               />
-              <h2 className="mt-2 font-semibold text-lg">{book.title}</h2>
+              <h2 className="mt-2 font-semibold text-sm">{book.title}</h2>
               <p className="text-sm text-gray-500">{book.author}</p>
             </div>
           </Link>
