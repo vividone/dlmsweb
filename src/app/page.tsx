764a470b8d4268fc0434ae10c3919504c15c762f
pascal <pascalokereke18@gmail.com>
2024-11-13 19:25:19 +0100

    page to sign up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,223 +1,188 @@
 "use client";
 
-import Image from "next/image";
-import Link from 'next/link';
-import { FaBell, FaSearch, FaFilter, FaBars } from "react-icons/fa";
-import { useState, useEffect, useRef } from 'react';
+import Link from "next/link";
+import { useState } from "react";
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
+
+export default function SignUp() {
+    const [fullname, setFullname] = useState<string>('');
+    const [email, setEmail] = useState<string>('');
+    const [homeAddress, setHomeAddress] = useState<string>('');
+    const [role, setRole] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const [error, setError] = useState<string | null>(null);
+    const [success, setSuccess] = useState<string | null>(null);
+    const [showPassword, setShowPassword] = useState<boolean>(false);
 
 
+    // password validation state
+    const [isUppercase, setIsUppercase] = useState<boolean>(false);
+    const [isLowercase, setIsLowercase] = useState<boolean>(false);
+    const [hasSpecialChar, setHasSpecialChar] = useState<boolean>(false);
 
-// Book type definition
-interface Book {
-  id: number;
-  title: string;
-  category: string;
-  cover: string;
-  author: string;
-}
+    // Check if password meets all requirements
+    const isPasswordValid = isUppercase && isLowercase && hasSpecialChar;
 
+    // Handle form submission
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        setError(null);
+        setSuccess(null);
 
-const books: Book[] = [
-  { id: 1, title: "Lone Wolf Adventure", category: "Sci-fi", author: 'Emerngard Nausicaa', cover: "/lone wolf.png" },
-  { id: 2, title: "Hide and Seek", category: "Fantasy", author: 'Olivia Wison', cover: "/Hide and seek.jpg" },
-  { id: 3, title: "Dont Look Back", category: "Drama", author: 'Isaac Nelson', cover: "/Dont Look.png" },
-  { id: 4, title: "Spring Book", category: "Romance", author: 'Deena Roberts', cover: "/spring book.jpg" },
-  { id: 5, title: "Harry Potter", category: "Business", author: 'Isaac Nelson', cover: "/harry potter.jpg" },
-  { id: 6, title: "A Promise Kept", category: "Fantasy", author: 'Robert Lee Hatcher', cover: "/Robin lee.jpg" },
-];
+        if (!fullname || !email || !homeAddress || !role || !password) {
+            setError("All fields are required.");
+            return;
+        }
+        if (!validateEmail(email)) {
+            setError("Please enter a valid email address.");
+            return;
+        }
+        if (!isPasswordValid) {
+            setError("Password must include an uppercase, a lowercase, and a special character.");
+            return;
+        }
 
-const booksTwo: Book[] = [
-  { id: 7, title: "Dont Look Back", category: "Drama", author: 'Isaac Nelson', cover: "/Dont Look.png" },
-  { id: 8, title: "Hide and Seek", category: "Fantasy", author: 'Olivia Wison', cover: "/Hide and seek.jpg" },
-  { id: 9, title: "Harry Potter", category: "Business", author: 'Isaac Nelson', cover: "/harry potter.jpg" },
-  { id: 10, title: "A Promise Kept", category: "Fantasy", author: 'Robert Lee Hatcher', cover: "/Robin lee.jpg" },
-  { id: 11, title: "Lone Wolf Adventure", category: "Sci-fi", author: 'Emerngard Nausica', cover: "/lone wolf.png" },
-  { id: 12, title: "Spring Book", category: "Romance", author: 'Deena Roberts', cover: "/spring book.jpg" },
-];
+            try {
+                const response = await fetch(`https://dlms-backend.onrender.com/auth/${role === "individual" ? "user" : "librarian"}/register`, {
+                    method: "POST",
+                    headers: {
+                        "Content-type": "application/json"
+                    },
+                    body: JSON.stringify({fullname, email, password, homeAddress, role, name: role === "individual" ? "user" : "librarian"})
+                })
 
-export default function Home() {
-  // State for search input and filtered books
-  const [searchTerm, setSearchTerm] = useState<string>('');
-  const [filteredBooks, setFilteredBooks] = useState<Book[]>([]);
-  const [selectedCategory, setSelectedCategory] = useState<string>('All');
-  const [dropdownOpen, setDropdownOpen] = useState(false);
-  const [menuOpen, setMenuOpen] = useState<boolean>(false);
-  const menuRef = useRef<HTMLDivElement>(null);
-  const dropdownRef = useRef<HTMLDivElement>(null);
+            if (response.ok) {
+                const data = await response.json();
+                setSuccess("Signup successful! Please log in.");
+                setFullname('');
+                setEmail('');
+                setHomeAddress('');
+                setRole('');
+                setPassword('');
+            } else {
+                const errorData = await response.json();
+                // Check for "Account already exists" message
+                if (errorData.error && errorData.error.includes("already exists")) {
+                    setError("An account with this email already exists. Please log in.");
+                } else {
+                    setError(errorData.error || "Signup failed. Please try again.");
+                }
+            } 
+        } catch (err) {
+            setError("An error occurred. Please try again later.");
+        } 
+    };
 
+    // Validate email format
+    const validateEmail = (email: string) => /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 
-   
-  // Handle search and filtering
-  useEffect(() => {
-    const filterData = (bookList: Book[]) =>
-      bookList.filter((book) => {
-        const matchesSearch =
-          book.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
-          book.author.toLowerCase().includes(searchTerm.toLowerCase());
-        const matchesCategory = selectedCategory === 'All' || book.category === selectedCategory;
-        return matchesSearch && matchesCategory;
-      });
+    // Validate password on input change
+    const handlePasswordChange = (value: string) => {
+        setPassword(value);
+        setIsUppercase(/[A-Z]/.test(value));
+        setIsLowercase(/[a-z]/.test(value));
+        setHasSpecialChar(/[!@#$%^&*(),.?":{}|<>]/.test(value));
+    };
 
-    const combinedFilteredBooks = [...filterData(books), ...filterData(booksTwo)];
-    setFilteredBooks(combinedFilteredBooks);
-  }, [searchTerm, selectedCategory]);
+    return (
+        <div className="flex flex-col items-center p-4 sm:p-8 overflow-x-hidden">
+            <h1 className="text-[#0661E8] text-4xl font-bold mt-8 sm:mt-16 mb-4">BookaThon</h1>
+            <h2 className="text-lg text-black mt-10 font-semibold sm:text-2xl mb-2">Hi, there!</h2>
+            <p className="text-sm text-gray-700 mb-8 text-center max-w-sm">Ready to join a family of leaders? Signup now</p>
 
+            <form onSubmit={handleSubmit} className="w-full max-w-md space-y-6">
+                {error && <div className="text-red-500 text-sm mb-4">{error}</div>}
+                {success && <div className="text-green-500 text-sm mb-4">{success}</div>}
 
-  useEffect(() => {
-    document.addEventListener("mousedown", handleClickOutside);
-      return () => document.removeEventListener("mousedown", handleClickOutside);
-  }, []);
+                {/* Full Name */}
+                <div className="space-y-1 text-black">
+                    <label className="block text-sm font-semibold">Full Name</label>
+                    <input
+                        type="text"
+                        value={fullname}
+                        onChange={(e) => setFullname(e.target.value)}
+                        placeholder="Input your full name"
+                        className="w-full px-4 py-3 border rounded-md"
+                        required
+                    />
+                </div>
 
-  const handleClickOutside = (event: MouseEvent) => {
-    // close dropdown if clicked
-    if(dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
-      setDropdownOpen(false);
-    }
-    // close menu if clicked outside
-    if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
-      setMenuOpen(false);
-    }
-  }
+                {/* Email Address */}
+                <div className="space-y-1 text-black">
+                    <label className="block text-sm font-semibold">Email Address</label>
+                    <input
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder="Enter your email address"
+                        className="w-full px-4 py-3 border rounded-md"
+                        required
+                    />
+                </div>
 
-  return (
-    <div className="container mx-auto p-4 overflow-x-hidden">
-      {/* Header */}
-      <header className="flex justify-between items-center sm:flex-row mb-8 space-y-4 sm:space-y-0">
-      <div className="flex items-center space-x-8">
-        <h1 className="text-3xl font-bold text-[#0661E8]">BookaThon</h1>
-          
-          {/*full nav links for larger screen */}
-          <nav className="hidden sm:flex space-x-6">
-          <Link href="/homepage" className="text-[#0061E8] text-base font-semibold sm:text-base hover:text-blue-500">
-            Library
-          </Link>
-          <Link href="/dashboard" className="font-semibold text-base sm:text-base hover:text-blue-500">
-            My Shelf
-          </Link>
-          </nav>
-  
+                {/* Home Address */}
+                <div className="space-y-1 text-black">
+                    <label className="block text-sm font-semibold">Home Address</label>
+                    <input
+                        type="text"
+                        value={homeAddress}
+                        onChange={(e) => setHomeAddress(e.target.value)}
+                        placeholder="Enter your home address"
+                        className="w-full px-4 py-3 border rounded-md"
+                        required
+                    />
+                </div>
 
+                 {/* Role */}
+                 <div className="space-y-1 text-black">
+                <label className="block text-sm font-semibold">Role</label>
+                  <select
+                   value={role}
+                   onChange={(e) => setRole(e.target.value)}
+                   className="w-full px-4 py-3 border rounded-md"
+                   required
+                >
+                <option value="" disabled>Select your role</option>
+                <option value="individual">Individual</option>
+                <option value="librarian">Librarian</option>
+                </select>
+                </div>
 
-    {/* Notification, Profile, and Hamburger Menu for mobile */}
-  <div className="flex items-center space-x-2 sm:space-x-4 absolute top-2 pr-6 right-0 sm:absolute top-2">
-    {/* Mobile hamburger menu */}
-    <div className="sm:hidden flex items-center text-black absolute top-5 right-20">
-      <FaBars 
-        className="text-md cursor-pointer" 
-        onClick={() => setMenuOpen(!menuOpen)} 
-      />
-         {/* Conditionally render the pop-up menu with smooth transition */}
-    {menuOpen && (
-      <div 
-        ref={menuRef}
-        className="absolute top-12 right-0 w-48 bg-white border rounded-md shadow-lg z-10 transition-all duration-300 transform opacity-100 scale-100"
-        style={{
-          opacity: menuOpen ? 1 : 0,
-          transform: menuOpen ? 'scale(1)' : 'scale(0.95)',
-        }}
-      >
-        <Link href="/homepage">
-          <div className="px-4 py-2 text-black hover:bg-gray-100 cursor-pointer">
-            Library
-          </div>
-        </Link>
-        <Link href="/dashboard">
-          <div className="px-4 py-2 text-black hover:bg-gray-100 cursor-pointer">
-            My Shelf
-          </div>
-        </Link>
-      </div>
-    )}
-    </div>
-    </div>
-    </div>
-
-
-        {/* Notification and Profile */}
-    
-          <div className="flex items-center space-x-2 sm:space-x-4 absolute top-2 pr-6 right-0 sm:absolute top-2">
-          <FaBell className='text-sm text-gray-700 cursor-pointer hover:text-blue-500' />
-          <Image  
-            src='/user-avatar.jpg'
-            alt="Avatar"
-            width={20}
-            height={10}
-            className="w-6 h-6 border rounded-full cursor-pointer"
-            onClick={() => setDropdownOpen(!dropdownOpen)}
-         />
-         {dropdownOpen && (
-          <div ref={dropdownRef} className="absolute right-0 mt-2 w-48 sm:right-0 text-sm bg-white border rounded-md shadow-lg">
-            <Link href="/sign-in">
-                  <div className="px-4 py-2 text-black hover:bg-gray-100 cursor-pointer">
-                    Sign Out
+                {/* Password */}
+                <div className="relative space-y-1 text-black">
+                    <label className="block text-sm font-semibold">Password</label>
+                    <input
+                        type={showPassword ? "text" : "password"}
+                        value={password}
+                        onChange={(e) => handlePasswordChange(e.target.value)}
+                        placeholder="Enter your password"
+                        className="w-full px-4 py-3 border rounded-md"
+                        required
+                    />
+                    <p className="text-xs font-semibold text-black mt-1">
+                        Password must include an uppercase, a lowercase, and a special character.
+                    </p>
+                    <div className="absolute inset-y-0 right-4 flex items-center cursor-pointer" onClick={() => setShowPassword(!showPassword)}>
+                        {showPassword ? <FaEyeSlash size={20} className="text-gray-600" /> : <FaEye size={20} className="text-gray-600" />}
                     </div>
-            </Link>
-          </div>
-         )}
-         </div>
-      </header>
+                </div>
 
-      {/* Find a Book */}
-      <div className="mb-6 ml-6 text-black">
-        <p className="text-xl font-semibold">Find a Book</p>
-      </div>
+                <button
+                    type="submit"
+                    className={`w-full py-3 ${isPasswordValid ? 'bg-[#0661E8]' : 'bg-blue-700'} text-white rounded-md cursor-pointer`}
+                    disabled={!isPasswordValid}
+                    aria-label="Signup"
+                >
+                    Submit
+                </button>
+            </form>
 
-      {/* Search and Filter */}
-      <div className="flex items-center mb-6 space-x-2 sm:space-x-4 md:space-x-4">
-        <div className="relative flex-grow">
-          <input 
-            type="text" 
-            value={searchTerm}
-            className="border text-sm text-black p-2 sm:p-3 md:p-4 w-full sm:w-3/4 md:w-full rounded-md focus:outline-none focus:ring-2 focus:ring-gray-800"      
-            placeholder="Type book name or author"
-            onChange={(e) => setSearchTerm(e.target.value)}
-          />
-          <FaSearch 
-            className="absolute text-sm right-3 top-1/2 transform -translate-y-1/2 text-gray-500 cursor-pointer" 
-          />
-        </div>
-        <button 
-          onClick={() => setSelectedCategory('All')}
-          className="flex items-center px-4 py-2 sm:px-6 sm:py-3 md:px-8 md:py-2 ml-2 text-white bg-[#0661E8] rounded-md hover:bg-blue-600"
-        > 
-          <FaFilter className="mr-2 text-sm opacity-40" /> {/* Filter Icon */}
-         <p className="text-sm text-white">Filter</p>
-        </button>
-      </div>
-
-      {/* Category Filter */}
-      <div className="flex overflow-x-auto flex lg:overflow-x-auto flex sm:overflow-visible gap-20 font-semibold sm:justify-between mb-6 animate-scroll">
-        {['All', 'Sci-fi', 'Fantasy', 'Romance', 'Drama', 'Business', 'Education', 'Geography'].map((category) => (
-          <button
-            key={category}
-            className={`px-4 py-2 whitespace-nowrap lg:whitespace-nowrap rounded-md text-sm  ${
-              selectedCategory === category ? 'bg-blue-600 text-white' : 'bg-blue-200 text-black'
-            }`}
-            onClick={() => setSelectedCategory(category)}
-          > 
-            {category}
-          </button>
-        ))}
-      </div>
-
-      {/* Book Collection */}
-      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-6 text-black">
-        {filteredBooks.map((book) => (
-          <Link key={book.id} href={`/book/${book.id}`}>
-            <div className="p-4 rounded-md hover:shadow-lg transition-shadow cursor-pointer">
-              <Image
-                src={book.cover}
-                alt={book.title}
-                width={192}
-                height={300}
-                className="rounded-md w-full h-auto"
-              />
-              <h2 className="mt-2 font-semibold text-black text-sm">{book.title}</h2>
-              <p className="text-sm text-black">{book.author}</p>
+            <div className="mt-6 text-sm text-center text-black">
+                <span>
+                    Already have an account?{" "}
+                    <Link href="/sign-in" className="text-[#0661E8] hover:text-blue-700">Login</Link>
+                </span>
             </div>
-          </Link>
-        ))}
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
