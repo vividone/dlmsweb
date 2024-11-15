3aebf477060a09c7f9a453c6a1daed136e8d160f
pascal <pascalokereke18@gmail.com>
2024-11-15 11:47:12 +0100

    borrow-page

--- src/app/borrowing-confirmation/page.tsx
+++ src/app/borrowing-confirmation/page.tsx
@@ -122,7 +122,7 @@ export default function BorrowingConfirmation(){
 
       {/* Book Details */}
       <div className="flex flex-col items-center p-8">
-      <Link href={'/borrowing-page'}>
+      <Link href={'/borrow-page'}>
           <FaArrowLeft className="text-lg  absolute sm:absolute left-2 absolute md:absolute left-16 text-gray-800 cursor-pointer hover:text-blue-500" />
         </Link>
 
