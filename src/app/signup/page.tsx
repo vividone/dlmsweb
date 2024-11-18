55abeff99fc216ee2b982b94d6f9c6b39a1bc24b
olaoluwa <olaoluwaayanbola@gmail.com>
2024-11-18 01:00:45 -0800

    feat:registration and login update

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -45,7 +45,7 @@ export default function SignUp() {
 
     try {
       const response = await fetch(
-        `https://dlms-backend.onrender.com/auth/${role}/register`,
+        `https://dlms-backend.onrender.com/auth/individual/register`,
         {
           method: "POST",
           headers: {
